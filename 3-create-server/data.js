62a6ee1cd8a35cb4920564bb804a128a57e5b4ec
Khubaib Jamal <khubaib.j18@gmail.com>
2024-09-05 00:50:11 +0500

    sample api and create server

---
+++ 3-create-server/data.js
@@ -0,0 +1,8 @@
+
+const data = [
+    { "Name": "Khubaib", "Developer": "Flutter Developer" },
+    { "Name": "Rehan", "Developer": "Web Developer" },
+    { "Name": "Muzzamil", "Developer": "React Development" },
+]
+
+module.exports = data
