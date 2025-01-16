4c902743d020bb5d01ab4119ff6787d9217c208b
Khubaib Jamal <khubaib.18@gmail.com>
2025-01-17 00:45:02 +0500

    ejs setup

--- 3-create-server/data.js
+++ 3-create-server/data.js
@@ -1,6 +1,10 @@
 
 const data = [
-    { "Name": "Khubaib", "Developer": "Flutter Developer" },
+    { "Name": "Khubaib", "Developer": "Flutter Developer", "skills":{
+        "frontend": "Flutter",
+        "backend": "Node js",
+        "service-side": "Firebase, MongoDB",
+    } },
     { "Name": "Rehan", "Developer": "Web Developer" },
     { "Name": "Muzzamil", "Developer": "React Development" },
 ]
