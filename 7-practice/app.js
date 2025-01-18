c742bc578e3737beb11913c7d8b092ab2d09a89d
Khubaib Jamal <khubaib.j18@gmail.com>
2025-01-18 19:39:48 +0500

    read and right file system with ejs

---
+++ 7-practice/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const fs = require('node:fs');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    fs.readdir(`./files`, (err, files) => {
+        res.render('index', { files: files });
+    });
+})
+
+app.post('/create', (req, res) => {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.description, (error) => {
+        res.redirect('/');
+    });
+})
+
+
+// port listening
+app.listen('5000', () => {
+    console.log("http://localhost:5000");
+})
