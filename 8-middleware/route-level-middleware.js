44b0c32b9192e9fdff3cc0ef5f4ae7552c5acd1d
Khubaib Jamal <khubaib.j18@gmail.com>
2025-06-30 00:50:30 +0500

    feat: resolve error

--- 8-middleware/route-level-middleware.js
+++ 8-middleware/route-level-middleware.js
@@ -6,11 +6,12 @@ const port = 5000;
 app.get('/', (req, res) => {
     res.send("Welcome");
 });
+
 app.get('/user', reqFilter, (req, res) => {
     res.send("welcome user");
 });
 
 app.listen(port, function (err) {
-    if (err) console.log("Error in server setup")
+    if (err) console.log(`Error in server setup: ${err}`);
     console.log(`http://localhost:${port}`);
 })
