676f3131304798b35b522a0a82b475d10d29810c
Khubaib Jamal <khubaib.j18@gmail.com>
2025-07-02 21:39:31 +0500

    feat:add env

--- 10-mongo-crud/config/db.js
+++ 10-mongo-crud/config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = "mongodb://localhost:27017/e-commerce";
+const MONGO_URI = process.env.MONGO_URI
 
 const connectDB = async () => {
     try {
         await mongoose.connect(MONGO_URI);
-        console.log("MongoDB connected");
+        console.log("💹 MongoDB connected");
         
     } catch (error) {
-        console.log("MongoDB connection error", error);
+        console.log("❌ MongoDB connection error", error);
     }
 };
 
--- 10-mongo-crud/index.js
+++ 10-mongo-crud/index.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDb = require('./config/db');
 const productModel = require('./models/products-model');
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDb();
 
