2d7dc064d8c22f7297665b091edb765795449be3
Khubaib Jamal <khubaib.j18@gmail.com>
2025-07-09 23:52:40 +0500

    feat:model class4

---
+++ 12-mongo-atlas-connector/models/products-model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    description: String,
+    category: String,
+    profilePic: String,
+}, {
+    timestamps: true,
+    toJSON: {
+        transform: (doc, ret) => {
+            ret.createdAt = ret.createdAt.toISOString();
+            ret.updatedAt = ret.updatedAt.toISOString();
+            return ret;
+        },
+    },
+});
+module.exports = mongoose.model('products', productSchema);
