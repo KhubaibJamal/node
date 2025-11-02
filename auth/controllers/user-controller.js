2319b2e605aea62476e51d1c5ad4f6650608b6f6
Khubaib Jamal <khubaib.j18@gmail.com>
2025-11-02 16:01:10 +0500

    feat:token expire

--- auth/controllers/user-controller.js
+++ auth/controllers/user-controller.js
@@ -50,11 +50,19 @@ const loginUser = async (req, res) => {
             return res.status(401).json({ message: "Invalid email or password" });
         }
 
+
+        const token = jwt.sign(
+            { id: existingUser._id, email: existingUser.email },
+            process.env.JWT_SECRET,
+            { expiresIn: process.env.JWT_EXPIRES_IN || '30d' }
+        );
+
         const userResponse = existingUser.toObject();
         delete userResponse.password;
 
         res.status(200).json({
             message: "Login successful",
+            token,
             user: userResponse
         });
 
